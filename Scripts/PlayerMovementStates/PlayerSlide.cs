91ac3b99890e62a7c08aa2684c66da8a88e6b3bb
sera5m <69703832+sera5m@users.noreply.github.com>
2026-09-14 19:56:37 -0500

    Source OnGround checks for slide, wallrun, vertical wallrun

--- Scripts/PlayerMovementStates/PlayerSlide.cs
+++ Scripts/PlayerMovementStates/PlayerSlide.cs
@@ -4,28 +4,21 @@ using System;
 [GlobalClass, Icon("res://addons/finite_state_machine/state_icon.png")]
 public partial class PlayerSlide : PlayerMovementState
 {
-    public static event Action SlideStartChange; // Event that fires whenever a slide has started
-	public static event Action<float> SlideCurrentChange; // Event that constantly fires returning the timer
+    public static event Action SlideStartChange;
+	public static event Action<float> SlideCurrentChange;
     private float _slopeTimer = 0f;
     private float _slopeTimerThreshold = 0.3f;
 
     public override void Enter()
     {
         base.Enter();
-
         SlideStartChange?.Invoke();
-
-        // Get slide momentum
         Movement.momentum = Movement.Velocity.Length();
-
         Movement.camState = CameraState.Freelooking;
-
         Movement.slideVector = Movement.inputDirection;
         Movement.slideBasis = Movement.Transform.Basis;
         Movement.initialRotationY = Movement.Rotation.Y;
-
         Movement.slideTimer = Movement.slideTimerMax;
-
         _slopeTimer = 0f;
     }
 
@@ -33,30 +26,20 @@ public partial class PlayerSlide : PlayerMovementState
     {
         Movement.slideTimer = 0f;
         Movement.camState = CameraState.Normal;
-
         _slopeTimer = 0f;
     }
 
     public override void PhysicsUpdate(double delta)
     {
         float floorAngle = Mathf.RadToDeg(Movement.GetFloorAngle());
-
         Vector3 slideVec = new Vector3(Movement.slideVector.X, 0f, Movement.slideVector.Y);
         Vector3 floorDirection = slideVec.Slide(Movement.GetFloorNormal());
-
         Movement.Crouch(delta);
-
-        // Handle slide timer
-        // Get the extra momentum from the slide
         Movement.direction = (Movement.slideBasis * floorDirection).Normalized();
-        
         Movement.currentSpeed = (Movement.slideTimer + 0.1f) * Movement.slideSpeed;
 
         if (floorAngle > 8f && !Movement.IsRunningUpSlope())
-        {
-            // Running down slope
             Movement.momentum += (float)delta * Movement.slideSpeed / 4;
-        }
         else
             Movement.slideTimer -= (float)delta;
 
@@ -67,24 +50,16 @@ public partial class PlayerSlide : PlayerMovementState
             EmitSignal(SignalName.StateFinished, "PlayerIdle", new());
 
         if (Movement.slideTimer <= 0f)
-        {
             EmitSignal(SignalName.StateFinished, "PlayerCrouch", new());
-        }
 
-        if (!Movement.IsOnFloor() && Mathf.Abs(Movement.Velocity.Y) > 0.1f)
-		{
-			EmitSignal(SignalName.StateFinished, "PlayerAir", new());
-		}
+        if (!Movement.OnGround && Mathf.Abs(Movement.Velocity.Y) > 0.1f)
+            EmitSignal(SignalName.StateFinished, "PlayerAir", new());
 
         if (Movement.CheckVault(delta, out Vector3 vaultPoint))
-        {
             EmitSignal(SignalName.StateFinished, "PlayerVault", new());
-        }
 
         if (Movement.CheckLadder())
-        {
             EmitSignal(SignalName.StateFinished, "PlayerLadder", new());
-        }
 
         SlideCurrentChange?.Invoke(Movement.slideTimer);
     }
--- Scripts/PlayerMovementStates/PlayerVerticalWallrun.cs
+++ Scripts/PlayerMovementStates/PlayerVerticalWallrun.cs
@@ -10,20 +10,14 @@ public partial class PlayerVerticalWallrun : PlayerMovementState
     public override void Enter()
     {
         base.Enter();
-
         VerticalWallRunStart?.Invoke();
-
         float desiredVelocity = Mathf.Sqrt(2 * Movement.gravity * Movement.verticalRunHeight);
         float time = Movement.verticalRunHeight / desiredVelocity;
-
         float height = 1/2 * Movement.gravity * (float)Math.Pow(time, 2) + desiredVelocity * time;
         GD.Print(height);
-
         Movement.currentSpeed = 0f;
-
         Movement.Jump(desiredVelocity);
         Movement.Velocity = Movement.playerVelocity;
-
         Movement.camState = CameraState.Wallrunning;
     }
 
@@ -31,7 +25,6 @@ public partial class PlayerVerticalWallrun : PlayerMovementState
     {
         VerticalWallRunEnd?.Invoke(this);
         Movement.camState = CameraState.Normal;
-
         Movement.currentSpeed = 3f;
         Movement.playerVelocity.Y = 0f;
     }
@@ -39,29 +32,18 @@ public partial class PlayerVerticalWallrun : PlayerMovementState
     public override void HandleInput(InputEvent @event)
     {
         if (@event is InputEventMouseMotion eventMouseMotion)
-        {
             Movement.RotatePlayerByConstraintUp(eventMouseMotion.Relative.X, eventMouseMotion.Relative.Y, -15f, 15f, 75f, 20f);
-        }
     }
 
     public override void PhysicsUpdate(double delta)
     {
         if (!Movement.SendRayInDirection(-Movement.GlobalBasis.Z, 0.5f, out Vector3 normal, out Vector3 point))
-        {
             EmitSignal(SignalName.StateFinished, "PlayerAir", new());
-        }
 
-        // Handle landing
-		if (Movement.IsOnFloor() || Movement.playerVelocity.Y < 0f)
-		{
+        if (Movement.OnGround || Movement.playerVelocity.Y < 0f)
             EmitSignal(SignalName.StateFinished, "PlayerAir", new());
-		}
 
         if (Movement.CheckVault(delta, out Vector3 vaultPoint))
-        {
             EmitSignal(SignalName.StateFinished, "PlayerVault", new());
-        }
-
-        
     }
 }
