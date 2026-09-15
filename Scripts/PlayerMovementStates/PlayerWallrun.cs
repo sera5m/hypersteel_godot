57c40b8774cd2ac49b91deba8b7d391c55e27c02
sera5m <69703832+sera5m@users.noreply.github.com>
2026-09-14 19:57:03 -0500

    Source OnGround for wallrun and ladder

--- Scripts/PlayerMovementStates/PlayerWallrun.cs
+++ Scripts/PlayerMovementStates/PlayerWallrun.cs
@@ -11,22 +11,16 @@ public partial class PlayerWallrun : PlayerMovementState
     public static Action<Node> WallRunEnd;
 
     private float _frictionForce;
-
     private KinematicCollision3D _collision;
     public String wallDirection;
     
     public override void Enter()
     {
         base.Enter();
-
         WallRunStart?.Invoke();
-
-        // Calculate friction force and give forward momentum
         _frictionForce = Movement.wallFrictionCoefficient * Movement.gravity; 
-
         Movement.momentum = Movement.Velocity.Length();
         Movement.camState = CameraState.Wallrunning;
-
         Movement.CheckWall(out _collision, out wallDirection);
     }
 
@@ -42,7 +36,6 @@ public partial class PlayerWallrun : PlayerMovementState
         {
             float rotationDegLeft = wallDirection == "Left" ? -60f : 0f;
             float rotationDegRight = wallDirection == "Right" ? 60f : 0f;
-
             Movement.RotatePlayerByConstraint(eventMouseMotion.Relative.X, eventMouseMotion.Relative.Y, rotationDegLeft, rotationDegRight);
         }
     }
@@ -50,7 +43,6 @@ public partial class PlayerWallrun : PlayerMovementState
     public override void PhysicsUpdate(double delta)
     {
         Vector3 rayDirection = Movement.GlobalBasis.X;
-
         if (wallDirection == "Left")
             rayDirection = -Movement.GlobalBasis.X; 
 
@@ -58,41 +50,18 @@ public partial class PlayerWallrun : PlayerMovementState
 
         if (wall && Movement.wallRunTimer <= Movement.wallRunTime)
         {
-            // Decrease wall run timer
             Movement.wallRunTimer += (float)delta;
-
             float normalizedTime = Movement.wallRunTimer / Movement.wallRunTime;
-
-            //Movement.momentum += (float)delta * Movement.wallRunSpeed / 4;
             Movement.currentSpeed = Mathf.Lerp(Movement.currentSpeed, Movement.wallRunSpeed, normalizedTime);
-
-            // Get forward vector of wall based on direction
             Vector3 rotatedVector = wallNormal.Rotated(Vector3.Up, Mathf.DegToRad(wallDirection == "Right" ? -90f : 90f));
-            
-
-            // Clamp rotation
-            //float angle = Movement.GlobalBasis.Z.SignedAngleTo(-rotatedVector, Vector3.Up);
-            float signedAngle = Mathf.RadToDeg(Movement.GlobalBasis.Z.SignedAngleTo(wallNormal, Vector3.Up));
-            //GD.Print(signedAngle);
-            
-            //Movement.RotateY(angle);
-
-            /* DebugDraw3D.DrawArrow(wallPoint, wallPoint + (rotatedVector * 1f), Colors.Aqua, 0.2f);
-            DebugDraw3D.DrawArrow(wallPoint + (rotatedVector * 1f), wallPoint + (wallNormal * 1f), Colors.IndianRed, 0.1f); */
-
-            // Decrease gravity with friction force over time
             Movement.playerVelocity.Y -= _frictionForce * (float)delta;
-
-            // Increase gravity over time
             _frictionForce = Mathf.Lerp(_frictionForce, Movement.gravity, normalizedTime / 4);
-
             Movement.direction = Movement.direction.Lerp(rotatedVector.Normalized(), 
                 1.0f - Mathf.Pow(0.5f, (float)delta * Movement.lerpSpeed));
 
             if (Input.IsActionJustPressed("jump"))
             {
-		        Movement.WallJump(wallNormal);
-
+                Movement.WallJump(wallNormal);
                 EmitSignal(SignalName.StateEntered, "PlayerAir", new());
             }
         }
@@ -101,16 +70,13 @@ public partial class PlayerWallrun : PlayerMovementState
             EmitSignal(SignalName.StateFinished, "PlayerAir", new());
         }
         
-        // Handle landing
-		if (Movement.IsOnFloor() 
+        if (Movement.OnGround 
         || (Movement.SendRayInDirection(-Movement.GlobalBasis.Z, 0.5f, out Vector3 normal, out Vector3 point)))
-		{
             EmitSignal(SignalName.StateFinished, "PlayerAir", new());
-		}
 
         if (Movement.CheckVault(delta, out Vector3 vaultPoint))
         {
-            Movement.wallRunTimer = 0f; // Reset the timer
+            Movement.wallRunTimer = 0f;
             EmitSignal(SignalName.StateFinished, "PlayerVault", new());
         }
 
