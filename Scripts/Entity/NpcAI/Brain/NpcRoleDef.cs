603e786f152f5299b4bab0b5a501519a8255a36f
sera5m <69703832+sera5m@users.noreply.github.com>
2026-09-24 00:16:28 -0500

    AI Brain: RoleDef engage bands + comms + fear crumb

--- Scripts/Entity/NpcAI/Brain/NpcRoleDef.cs
+++ Scripts/Entity/NpcAI/Brain/NpcRoleDef.cs
@@ -9,8 +9,12 @@ public partial class NpcRoleDef : Resource
 	[Export] public string RoleId = "soldier";
 	[Export] public NpcVerb DefaultVerb = NpcVerb.MoveAndAttack;
 	[Export] public float PreferredRange = 18f;
+	[Export] public float CloseRange = 8f;
+	[Export] public float FarRange = 28f;
 	[Export] public bool WantsHighGround;
 	[Export] public bool SoftStep;
 	[Export] public bool IssuesEnclose;
 	[Export] public bool PingsOnSight;
+	[Export] public bool HasTeamComms;
+	[Export] public float FearResist = 0.2f;
 }
